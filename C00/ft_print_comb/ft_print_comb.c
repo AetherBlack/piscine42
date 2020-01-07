a23752581ae3820252555c7e722567c7083d31db
Black Aether <52674895+AetherBlack@users.noreply.github.com>
2020-01-07 22:04:35 +0100

    Update ft_print_comb.c

--- C00/ft_print_comb/ft_print_comb.c
+++ C00/ft_print_comb/ft_print_comb.c
@@ -1,31 +1,41 @@
 #include <unistd.h>
 
-void ft_print_comb(void) {
+void ft_print_comb(void)
+{
 
-    int nmb1 = 48;
-    int nmb2 = 49;
-    int nmb3 = 50;
+    char nmb1 = '0';
+    char nmb2 = '1';
+    char nmb3 = '2';
 
-    while (nmb1 != 55) {
+    while (nmb1 != '8')
+    {
+
         write(1, &nmb1, 1);
         write(1, &nmb2, 1);
         write(1, &nmb3, 1);
-        write(1, ", ", 2);
+        
+        if (nmb1 != '7' || nmb2 != '8' || nmb3 != '9')
+        {
+            write(1, ", ", 2);
+        }
 
         nmb3++;
-        
-        if (nmb3 == 58) {
+
+        if (nmb3 > '9')
+        {
             nmb2++;
-            nmb3 = 50;
-        }
-        if (nmb2 == 57) {
-            nmb1++;
-            nmb2 = 49;
+            if (nmb2 == '9')
+            {
+                nmb1++;
+                nmb2 = nmb1 + 1;
+            }
+            nmb3 = nmb2 + 1;
         }
     }
 }
 
-int main(void) {
+int main(void)
+{
     ft_print_comb();
-    return 0;
+    return(0);
 }
