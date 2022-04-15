cbe55f68c407999e7701c30c262fe8de9874fb8b
AetherBlack <aetherblack@github.com>
2022-04-15 04:29:19 -0400

    new(ft_strcat) : Add ft_strcat function

---
+++ C03/ft_strcat/ft_strcat.c
@@ -0,0 +1,22 @@
+
+char *ft_strcat(char *dest, char *src)
+{
+    while (*dest)
+        dest++;
+
+    for (; *src; src++, dest++)
+        *dest = *src;
+
+    *dest = 0x0;
+
+    return dest;
+}
+
+int main(void)
+{
+    char src[50] = "This is source", dest[50] = "This is destination";
+
+   ft_strcat(dest, src);
+   
+   return 0;
+}
